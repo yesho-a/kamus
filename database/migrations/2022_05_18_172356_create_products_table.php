4c78762496e89d874af585552d0391f989ac35a2
Yesho Simon <sepkap123@gmail.com>
2022-05-18 21:32:03 +0300

    added product sschema

--- database/migrations/2022_05_18_172356_create_products_table.php
+++ database/migrations/2022_05_18_172356_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description');
+            $table->float('price');
             $table->timestamps();
         });
     }
